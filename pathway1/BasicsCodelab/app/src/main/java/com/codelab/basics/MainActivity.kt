f6e64f54ff7b419b7d53a502886a1e4c9ab91226
Hyeon_ <onhyeon618@gmail.com>
2022-11-16 00:40:47 +0900

    12. Styling and theming your app

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,6 +23,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
@@ -145,7 +147,13 @@ private fun Greeting(name: String) {
             ) {
                 // child element: Flutter와 달리 단순히 나열하여 사용 가능
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    // MaterialTheme에 지정해 둔 값 사용 가능 + copy를 활용하여 수정 사용 가능
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -156,11 +164,16 @@ private fun Greeting(name: String) {
     }
 }
 
-// 프리뷰 화면의 크기 설정 가능 -> widthDp, heightDp
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
