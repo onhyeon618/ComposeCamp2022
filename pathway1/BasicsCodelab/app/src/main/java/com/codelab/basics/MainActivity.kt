26a8a0bd81fd98652469fb3e324bd422bf893fce
Hyeon_ <onhyeon618@gmail.com>
2022-11-15 20:35:30 +0900

    7. State in Compose

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,25 +45,38 @@ fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
-    // backgroundColor가 primary가 될 때, 텍스트 컬러는 자동으로 onPrimary 사용
+    // state 추가하기:
+
+    // var expanded = false -> 이런 방식으로는 동작하지 않는다.
+    // 1. it's not being tracked by Compose
+    // 2. Greeting()이 호출될 때마다 expanded는 false로 다시 초기화될 것.
+
+    // remember: recomposition 될 때마다 값을 다시 부여받지 않도록, 자신의 값을 보존하게 함
+    // mutableStateOf(): composable이 사용하는 state로 지정
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         // modifier: UI 요소의 외형 및 동작 방식 규정 ... "To add multiple modifiers to an element, you simply chain them."
         Row(modifier = Modifier.padding(24.dp)) {
-            // fillMaxWidth(): 너비를 최대로 채우도록 지정할 때 사용 / weight를 사용하면 불필요
-            // Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 // child element: Flutter와 달리 단순히 나열하여 사용 가능
-                // default 동작: each row takes the minimum space it can
                 Text(text = "Hello,")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                // 함수를 전달!!
+                onClick = { expanded.value = !expanded.value },
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
