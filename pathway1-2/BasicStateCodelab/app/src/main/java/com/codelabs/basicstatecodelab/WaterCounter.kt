a66d0279dce930dff023bcc694eb5611c5401435
Hyeon_ <onhyeon618@gmail.com>
2022-12-04 02:22:32 +0900

    5. Memory in a composable function

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -2,17 +2,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    // 여기부터는 슬슬 Flutter와 비슷해지네...
-    // 전반적으로 내용이 눈에 익은 방식이다.
     Column(modifier = modifier.padding(16.dp)) {
-        // 아래 방식은 예상대로 동작하지 않는다.
-        var count = 0
+        // val count: MutableState<Int> = remember { mutableStateOf(0) }
+        // -> 사용할 때는 count.value++와 같이(처럼) 사용
+
+        // 위 방식을 보다 간소하게 사용하려면 다음과 같이 쓸 수 있다:
+        var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
