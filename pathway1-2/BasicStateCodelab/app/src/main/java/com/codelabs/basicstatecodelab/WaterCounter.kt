ae1b0c2c214de3cbf74d5c62379ac9ccef22cd82
Hyeon_ <onhyeon618@gmail.com>
2022-12-04 01:55:45 +0900

    4. Events in Compose

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,4 +1,6 @@
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -6,9 +8,14 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    // 여기부터는 슬슬 Flutter와 비슷해지네...
+    // 전반적으로 내용이 눈에 익은 방식이다.
+    Column(modifier = modifier.padding(16.dp)) {
+        // 아래 방식은 예상대로 동작하지 않는다.
+        var count = 0
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
