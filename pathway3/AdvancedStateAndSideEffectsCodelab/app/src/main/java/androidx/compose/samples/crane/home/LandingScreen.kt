d3172f10975539de251b2ff8a7d830a89a0d22fb
Nahyeon Oh <onhyeon618@gmail.com>
2022-12-13 00:46:45 +0900

    4. LaunchedEffect and rememberUpdatedState

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
-fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
+fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {   // 한때 SplashScreen이었던 것들...
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // composable 안에서 suspend 함수를 안전하게 호출하려면 LaunchedEffect를 사용한다 -> Coroutine에서 실행
+        // LaunchedEffect(value): value(key)가 바뀔 때마다 재실행된다. 한 번만 실행하려면 true 등의 상수를 줄 것
+        // 이때 계속 최신 값을 체크하고 받아야 하는 게 있으면, 위의 currentOnTimeout처럼 rememberUpdatedState를 쓴다.
+        LaunchedEffect(true) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
