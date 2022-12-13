02bbbac437be60f0b6d43fc2b91c48ab2ce846ed
Hyeon_ <onhyeon618@gmail.com>
2022-12-14 02:44:53 +0900

    3. Create a simple UI test

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,35 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        // 우리가 컨트롤할 수 있는 더미데이터
+        val allScreens = RallyScreen.values().toList()
+
+        // setContent를 이용하여 어떤 compose를 테스트할 건지 설정
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        // composeTestRule{.finder}{.assertion}{.action}
+        // 테스트는 다음의 규칙에 따라서 진행된다.
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+}
